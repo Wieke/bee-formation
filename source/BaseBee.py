1f55f108d459d82f649a53bc24a37e6225ddbc96
Mike Ligthart <mike.ligthart@gmail.com>
2014-03-11 13:29:39 +0100

    BaseBee

---
+++ source/BaseBee.py
@@ -0,0 +1,27 @@
+import abc
+
+class BaseBee:
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def arguments(self):
+        """Return all the arguments given to the bee."""
+        return
+
+    @abc.abstractmethod
+    def move(self, perception):
+        """Move according to the perception"""
+        return
+
+    @abc.abstractmethod
+    def shortRangeCommunicate(self, perception):
+        """Sent out short range communication message"""
+        return
+
+    @abc.abstractmethod
+    def name(self):
+        """Return name of the type of bee"""
+        return
+        
+
+        
