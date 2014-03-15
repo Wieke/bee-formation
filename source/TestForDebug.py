27bf6c49199a10a012c2dfbd25ba50d06c777f94
Mike Ligthart <mike.ligthart@gmail.com>
2014-03-15 19:56:34 +0100

    _accesableShortRangeCommunication is debuged and working properly

--- source/TestForDebug.py
+++ source/TestForDebug.py
@@ -5,3 +5,12 @@ form.selectedbeeclass = form.beeclasses[1]
 form.beearguments = form.selectedbeeclass.arguments()
 form.checkbeearguments()
 form.preparetheworld()
+form.world.stepForward()
+states = []
+states.append(form.world.getworldState())
+positions, bees, movement, communication = map(list, zip(*states[0]))
+ownPos = positions[7]
+otherPos = positions[:7] + positions[8:]
+otherCom = communication[:7] + communication[8:]
+trans = bees[7].transformation
+form.world._accesableShortRangeCommunication(ownPos, otherPos, otherCom, trans)
